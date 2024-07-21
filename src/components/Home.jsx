8d14aefba4568561f8c7bb95860b4f0cb6c53526
Lila Amelia <lilaamelia77@gmail.com>
2024-07-21 17:25:36 +0700

    videos added

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,99 +1,49 @@
-import video_thumbnail from '../assets/video-thumbnail.png'
+import { videos } from '../data/data'
 import './Global.css'
 
 const Home = () => {
-  return (
-    <div id="home">
-        <div className="container">
-            <div className="row my-3">
-                <div className="col" id="recommended">
-                    <h6 className="display-6">Recommended For You</h6>
-                    <div className="d-flex flex-wrap justify-content-center">
-                        <div className="item d-flex flex-column">
-                            <img src={video_thumbnail} alt="thumbnail.png" className="img-fluid" />
-                            <span className="lead fw-bold mt-2">Butter Chicken Meatballs For Workout</span>
-                            <span className="lead">Tasty</span>
-                            <span className="lead mb-2">2.6M views • 1 year ago</span>
-                        </div>
-                        <div className="item d-flex flex-column">
-                            <img src={video_thumbnail} alt="thumbnail.png" className="img-fluid" />
-                            <span className="lead fw-bold mt-2">Butter Chicken Meatballs For Workout</span>
-                            <span className="lead">Tasty</span>
-                            <span className="lead mb-2">2.6M views • 1 year ago</span>
-                        </div>
-                        <div className="item d-flex flex-column">
-                            <img src={video_thumbnail} alt="thumbnail.png" className="img-fluid" />
-                            <span className="lead fw-bold mt-2">Butter Chicken Meatballs For Workout</span>
-                            <span className="lead">Tasty</span>
-                            <span className="lead mb-2">2.6M views • 1 year ago</span>
-                        </div>
-                        <div className="item d-flex flex-column">
-                            <img src={video_thumbnail} alt="thumbnail.png" className="img-fluid" />
-                            <span className="lead fw-bold mt-2">Butter Chicken Meatballs For Workout</span>
-                            <span className="lead">Tasty</span>
-                            <span className="lead mb-2">2.6M views • 1 year ago</span>
-                        </div>
-                        <div className="item d-flex flex-column">
-                            <img src={video_thumbnail} alt="thumbnail.png" className="img-fluid" />
-                            <span className="lead fw-bold mt-2">Butter Chicken Meatballs For Workout</span>
-                            <span className="lead">Tasty</span>
-                            <span className="lead mb-2">2.6M views • 1 year ago</span>
-                        </div>
-                        <div className="item d-flex flex-column">
-                            <img src={video_thumbnail} alt="thumbnail.png" className="img-fluid" />
-                            <span className="lead fw-bold mt-2">Butter Chicken Meatballs For Workout</span>
-                            <span className="lead">Tasty</span>
-                            <span className="lead mb-2">2.6M views • 1 year ago</span>
+    const recVideo = videos.filter(video => (video.category).includes("recommended"))
+    const recList = recVideo.map((video) => (
+        <div className="item d-flex flex-column" key={video.id}>
+            <img src={video.thumbnail} alt="thumbnail.png" className="img-fluid" />
+            <span className="lead fw-bold mt-2">{video.title}</span>
+            <span className="lead">{video.channel}</span>
+            <span className="lead mb-2">{video.views} views • {video.uploadTime}</span>
+        </div>
+    ))
+    
+    const weekVideo = videos.filter(video => (video.category).includes("weekly"))
+    const weekList = weekVideo.map((video) => (
+        <div className="item d-flex flex-column" key={video.id}>
+            <img src={video.thumbnail} alt="thumbnail.png" className="img-fluid" />
+            <span className="lead fw-bold mt-2">{video.title}</span>
+            <span className="lead">{video.channel}</span>
+            <span className="lead mb-2">{video.views} views • {video.uploadTime}</span>
+        </div>
+    ))
+
+    return (
+        <div id="home">
+            <div className="container">
+                <div className="row my-3">
+                    <div className="col" id="recommended">
+                        <h6 className="display-6">Recommended For You</h6>
+                        <div className="d-flex flex-wrap justify-content-center">
+                            {recList}
                         </div>
                     </div>
                 </div>
-            </div>
-            <div className="row my-3">
-                <div className="col" id="weekly">
-                    <h6 className="display-6">This Week</h6>
-                    <div className="d-flex flex-wrap justify-content-center">
-                        <div className="item d-flex flex-column">
-                            <img src={video_thumbnail} alt="thumbnail.png" className="img-fluid" />
-                            <span className="lead fw-bold mt-2">Butter Chicken Meatballs For Workout</span>
-                            <span className="lead">Tasty</span>
-                            <span className="lead mb-2">2.6M views • 1 year ago</span>
-                        </div>
-                        <div className="item d-flex flex-column">
-                            <img src={video_thumbnail} alt="thumbnail.png" className="img-fluid" />
-                            <span className="lead fw-bold mt-2">Butter Chicken Meatballs For Workout</span>
-                            <span className="lead">Tasty</span>
-                            <span className="lead mb-2">2.6M views • 1 year ago</span>
-                        </div>
-                        <div className="item d-flex flex-column">
-                            <img src={video_thumbnail} alt="thumbnail.png" className="img-fluid" />
-                            <span className="lead fw-bold mt-2">Butter Chicken Meatballs For Workout</span>
-                            <span className="lead">Tasty</span>
-                            <span className="lead mb-2">2.6M views • 1 year ago</span>
-                        </div>
-                        <div className="item d-flex flex-column">
-                            <img src={video_thumbnail} alt="thumbnail.png" className="img-fluid" />
-                            <span className="lead fw-bold mt-2">Butter Chicken Meatballs For Workout</span>
-                            <span className="lead">Tasty</span>
-                            <span className="lead mb-2">2.6M views • 1 year ago</span>
-                        </div>
-                        <div className="item d-flex flex-column">
-                            <img src={video_thumbnail} alt="thumbnail.png" className="img-fluid" />
-                            <span className="lead fw-bold mt-2">Butter Chicken Meatballs For Workout</span>
-                            <span className="lead">Tasty</span>
-                            <span className="lead mb-2">2.6M views • 1 year ago</span>
-                        </div>
-                        <div className="item d-flex flex-column">
-                            <img src={video_thumbnail} alt="thumbnail.png" className="img-fluid" />
-                            <span className="lead fw-bold mt-2">Butter Chicken Meatballs For Workout</span>
-                            <span className="lead">Tasty</span>
-                            <span className="lead mb-2">2.6M views • 1 year ago</span>
+                <div className="row my-3">
+                    <div className="col" id="weekly">
+                        <h6 className="display-6">This Week</h6>
+                        <div className="d-flex flex-wrap justify-content-center">
+                            {weekList}
                         </div>
                     </div>
                 </div>
             </div>
         </div>
-    </div>
-  )
+    )
 }
 
 export default Home
---
+++ src/data/data.js
@@ -0,0 +1,159 @@
+export const videos = [
+    {
+        thumbnail: "https://i3.ytimg.com/vi/Rq1taW6LVOI/maxresdefault.jpg",
+        url: "https://www.youtube.com/watch?v=Rq1taW6LVOI",
+        title: "1-week Bento Prep for Weight Loss | Lost 10kg in 2 Months | 30g Protein per Meal | No Exercise",
+        channel: "ayahare diet",
+        views: "447K",
+        uploadTime: "1 month ago",
+        category: "recommended",
+    },
+    {
+        thumbnail: "https://i3.ytimg.com/vi/nnHrPhMkeQw/maxresdefault.jpg",
+        url: "https://www.youtube.com/watch?v=nnHrPhMkeQw",
+        title: "5 Popcorn Chicken Recipes For Your Watching Session",
+        channel: "Tasty",
+        views: "746K",
+        uploadTime: "5 years ago",
+        category: "recommended",
+        comments: [
+            {
+                user: "Chloe",
+                comment: "*Has a full meal* watches this video and gets hungry again :'(",
+                replies: [
+                    {
+                        user: "Casey",
+                        comment: "Me too"
+                    },
+                    {
+                        user: "Winston",
+                        comment: "Me three"
+                    },
+                    {
+                        user: "Lila",
+                        comment: "Y'all weak as heck"
+                    },
+
+                ]
+            },
+            {
+                user: "John",
+                comment: "The easiest chicken recipes I've ever seen",
+                replies: [
+                    {
+                        user: "Irfan",
+                        comment: "Agreed"
+                    },
+                    {
+                        user: "Andrew",
+                        comment: "I wouldn't really call these the easiest"
+                    },
+                ]
+            },
+            {
+                user: "John",
+                comment: "these would not last 5 minutes, let alone my binge watching session.",
+                replies: [
+                    {
+                        user: "Casey",
+                        comment: "not even 5 seconds"
+                    },
+                    {
+                        user: "Jake",
+                        comment: "LOL"
+                    },
+                ]
+            },
+        ]
+    },
+    {
+        thumbnail: "https://i3.ytimg.com/vi/A9D6v9hgYv8/maxresdefault.jpg",
+        url: "https://www.youtube.com/watch?v=A9D6v9hgYv8",
+        title: "6 EASY CHICKEN MARINADES | amazing chicken breast recipe + freezer friendly meal prep",
+        channel: "Downshiftology",
+        views: "1.5M",
+        uploadTime: "2 years ago",
+        category: "recommended",
+    },
+    {
+        thumbnail: "https://i3.ytimg.com/vi/7BBKC67cXL0/maxresdefault.jpg",
+        url: "https://www.youtube.com/watch?v=7BBKC67cXL0&t=4s",
+        title: "Cabbage with eggs is better than pizza! Easy, quick and very delicious recipe!",
+        channel: "Essen Recipes",
+        views: "10M",
+        uploadTime: "4 months ago",
+        category: "recommended",
+    },
+    {
+        thumbnail: "https://i3.ytimg.com/vi/6aYlzMiAOe0/maxresdefault.jpg",
+        url: "https://www.youtube.com/watch?v=6aYlzMiAOe0",
+        title: "How To Turn $1 Canned Tuna Into a Restaurant Meal (4 Ways)",
+        channel: "KWOOWK",
+        views: "1.3M",
+        uploadTime: "2 years ago",
+        category: "recommended",
+    },
+    {
+        thumbnail: "https://i3.ytimg.com/vi/PRH64zvMs0w/maxresdefault.jpg",
+        url: "https://www.youtube.com/watch?v=PRH64zvMs0w",
+        title: "Soft and fluffy homemade donut recipe/세상 부드러운 도넛",
+        channel: "마미오븐 MOMMY OVEN",
+        views: "7.3M",
+        uploadTime: "3 years ago",
+        category: "recommended",
+    },
+    {
+        thumbnail: "https://i3.ytimg.com/vi/HSwsnTjE0uc/maxresdefault.jpg",
+        url: "https://www.youtube.com/watch?v=HSwsnTjE0uc",
+        title: "The Only High Protein Pizza Recipe You’ll Ever Need | Freezer Meal Prep",
+        channel: "Ethan Paff",
+        views: "1.5K",
+        uploadTime: "17 hours ago",
+        category: "weekly",
+    },
+    {
+        thumbnail: "https://i3.ytimg.com/vi/fsLrAumSeDA/maxresdefault.jpg",
+        url: "https://www.youtube.com/watch?v=fsLrAumSeDA",
+        title: "Chilled and refreshingly delicious Nambanzuke chicken breast",
+        channel: "HidaMari Cooking",
+        views: "15K",
+        uploadTime: "1 day ago",
+        category: "weekly",
+    },
+    {
+        thumbnail: "https://i3.ytimg.com/vi/gzN0HNggQDI/maxresdefault.jpg",
+        url: "https://www.youtube.com/watch?v=gzN0HNggQDI",
+        title: "Peach Jelly Cheesecake Recipe",
+        channel: "Cooking tree 쿠킹트리",
+        views: "18K",
+        uploadTime: "2 days ago",
+        category: "weekly",
+    },
+    {
+        thumbnail: "https://i3.ytimg.com/vi/4QcK3MXl9sg/maxresdefault.jpg",
+        url: "https://www.youtube.com/watch?v=4QcK3MXl9sg",
+        title: "The Greatest Pancake Recipe of All Time (The GOAT)",
+        channel: "Hearty and tasty",
+        views: "144K",
+        uploadTime: "2 days ago",
+        category: "weekly",
+    },
+    {
+        thumbnail: "https://i3.ytimg.com/vi/3VQBfc6xb_k/maxresdefault.jpg",
+        url: "https://www.youtube.com/watch?v=3VQBfc6xb_k",
+        title: "I cook this pasta every day! An easy, simple and very tasty recipe!",
+        channel: "Hearty and tasty",
+        views: "677K",
+        uploadTime: "3 days ago",
+        category: "weekly",
+    },
+    {
+        thumbnail: "https://i3.ytimg.com/vi/W9HiDhYl0gU/maxresdefault.jpg",
+        url: "https://www.youtube.com/watch?v=W9HiDhYl0gU",
+        title: "Twice as Crispy as Grilled Corn Cheese Sandwich Recipe :: Easy Toast Recipe",
+        channel: "매일맛나 delicious day",
+        views: "2.8K",
+        uploadTime: "4 days ago",
+        category: "weekly",
+    },
+]
